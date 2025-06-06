2ed3d12f1783fb596e85b79b42180a72f44e8984
Leandro Carone Lanschi <leandrocala2@gmail.com>
2025-06-06 15:32:53 -0300

    [feat] Captura dos campos

---
+++ logica.js
@@ -0,0 +1,12 @@
+
+const input = document.getElementById('campoInput');
+
+const btnPesquisar = document.querySelector('.buttonPesquisar');
+
+const campoCity = document.getElementById('city');
+
+const campoGrau = document.getElementById('degree');
+
+const content = document.querySelector('.content');
+
+const img = document.querySelector('.campoImg');
