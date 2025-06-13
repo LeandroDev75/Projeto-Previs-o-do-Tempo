0658369c2304b418e7e1fe0cc3a7e9407eaee83a
Leandro Carone Lanschi <leandrocala2@gmail.com>
2025-06-13 11:13:50 -0300

    [feat] Criação da velocidade do vento.

--- logica.js
+++ logica.js
@@ -34,7 +34,36 @@ imageClima.src = icon;
 
 const descricaoTempo = document.querySelector('.descriçãoTempo');
 descricaoTempo.innerHTML = resp_json.weather[0].description;
- 
+
+const temp_min = document.querySelector('.min-temp');
+temp_min.innerHTML = `${resp_json.main.temp_min}<sup>°C</sup>`;
+
+const temp_max = document.querySelector('.max-temp');
+temp_max.innerHTML = `${resp_json.main.temp_max}<sup>°C</sup>`;
+
+const temp_feels = document.querySelector('.feels-temp');
+temp_feels.innerHTML = `${resp_json.main.feels_like}<sup>°C</sup>`;
+
+const temp_humidity = document.querySelector('.humidity-temp');
+temp_humidity.innerHTML = `${resp_json.main.humidity}%`;
+
+const temp_pressure = document.querySelector('.pressure-temp');
+temp_pressure.innerHTML = resp_json.main.pressure
+
+const temp_wind = document.querySelector('.wind-temp');
+
+const resp_wind = resp_json.wind.deg;
+const wind = resp_json;
+const value_wind = '';
+
+console.log(resp_wind);
+
+if (resp_wind >= 202.5 && resp_wind <= 247.5) {
+    temp_wind.innerHTML = 'SW';
+}
+
+const wind_speed = document.querySelector('.speed-wind');
+wind_speed.innerHTML = `${resp_json.wind.speed} Km/h`;
 
 } 
 chamarApi();
